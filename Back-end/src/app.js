fbdc563c3e4a841fc1894339f8ebb87e84af390d
xltsignosis <jricardo.cruz.ag@gmail.com>
2026-02-17 20:00:45 -0600

    Commit con codigos HTTP 200, 400, 404, 500)

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -3,6 +3,7 @@ const app = express();
 
 app.use(express.json());
 
+//http 200
 app.get('/api/items', (req, res) =>{
     res.json([{ id: 1, name: 'Elemento A', status: 'Activo'}, { id: 2, name: 'Elemento B', status: 'Inactivo'}]);
 })
@@ -17,6 +18,38 @@ app.get('/api/state-ok', (req, res) => {
 
 app.get('/api/state-error', (req, res) => {
     res.status(500).json({ status: 'error', message: 'fallo al cambiar el estado' });
+});
+
+// Simula una respuesta 200 con retraso para probar el manejo de tiempos de espera
+app.get('/api/items-delay', (req, res) => {
+    setTimeout(() => {
+        res.json([{ id: 1, name: 'Elemento A', status: 'Activo'}, { id: 2, name: 'Elemento B', status: 'Inactivo'}]);
+    }, 2000);
+});
+
+// Simula un error de red para probar el manejo de errores de red
+app.get('api/network-error', (req, res) => {
+    res.status(503).json({ message: 'servicio no disponible'});
+});
+
+// Simula una respuesta intermitente para probar el manejo de errores intermitentes
+app.get('/api/flaky', (req, res) => {
+    const falla = Math.random() < 0.5;
+    if ( falla) {
+        res.status(500).json({ message: 'error en el servidor' });
+    } else {
+        res.json({ data: 'datos cargados correctamente' });
+    } 
+});
+
+//error 400 controlado para el cliente
+app.get('/api/bad-request', (req, res) => {
+    res.status(400).json({ message: 'solicitud invalida'});
+});
+
+//error 404 controlado
+app.get('/api/not-found', (req, res) => {
+    res.status(404).json({ message: 'recurso no encontrado'});
 });
 
 module.exports = app;
