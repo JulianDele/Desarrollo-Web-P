969910b7834260d91f943f8b7040894e68235826
osmar200 <156713170+osmar200@users.noreply.github.com>
2026-03-15 17:39:12 -0600

    Implementación backend de autenticación con Express, JWT y middlewares de seguridad

--- Back-end/src/app.js
+++ Back-end/src/app.js
@@ -1,37 +1,39 @@
 const express = require('express');
+const cors = require('cors');
+
+const authRoutes = require('./routes/authRoutes');
+
 const app = express();
-
 app.use(express.json());
-
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
+// Rutas de autenticación
+app.use('/api', authRoutes);
 //http 200
 app.get('/api/items', (req, res) =>{
     res.json([{ id: 1, name: 'Elemento A', status: 'Activo'}, { id: 2, name: 'Elemento B', status: 'Inactivo'}]);
 })
-
 app.get('/api/error', (req, res) =>{
     res.status(500).json({ message: 'Error interno del servidor' });
 })
-
 app.get('/api/state-ok', (req, res) => {
     res.json({ status: 'activo', selected: true });
 });
-
 app.get('/api/state-error', (req, res) => {
     res.status(500).json({ status: 'error', message: 'fallo al cambiar el estado' });
 });
-
 // Simula una respuesta 200 con retraso para probar el manejo de tiempos de espera
 app.get('/api/items-delay', (req, res) => {
     setTimeout(() => {
         res.json([{ id: 1, name: 'Elemento A', status: 'Activo'}, { id: 2, name: 'Elemento B', status: 'Inactivo'}]);
     }, 2000);
 });
-
 // Simula un error de red para probar el manejo de errores de red
 app.get('/api/network-error', (req, res) => {
     res.status(503).json({ message: 'servicio no disponible'});
 });
-
 // Simula una respuesta intermitente para probar el manejo de errores intermitentes
 app.get('/api/flaky', (req, res) => {
     const falla = Math.random() < 0.5;
@@ -41,15 +43,12 @@ app.get('/api/flaky', (req, res) => {
         res.json({ data: 'datos cargados correctamente' });
     } 
 });
-
 //error 400 controlado para el cliente
 app.get('/api/bad-request', (req, res) => {
     res.status(400).json({ message: 'solicitud invalida'});
 });
-
 //error 404 controlado
 app.get('/api/not-found', (req, res) => {
     res.status(404).json({ message: 'recurso no encontrado'});
 });
-
 module.exports = app;
