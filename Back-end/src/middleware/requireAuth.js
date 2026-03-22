47e7c5cd9635ba663db83cd8925299d21021e7b1
osmar200 <156713170+osmar200@users.noreply.github.com>
2026-03-22 05:00:52 -0600

    Middleware para validación de JWT y sesión activa

--- Back-end/src/middleware/requireAuth.js
+++ Back-end/src/middleware/requireAuth.js
@@ -1,6 +1,7 @@
 const { verifyToken } = require('../utils/token');
+const Session = require('../models/Session');
 
-module.exports = (req, res, next) => {
+module.exports = async (req, res, next) => {
 
     const authHeader = req.headers.authorization;
     if (!authHeader) {
@@ -9,7 +10,18 @@ module.exports = (req, res, next) => {
     const token = authHeader.split(" ")[1];
     try {
         const decoded = verifyToken(token);
+        const session = await Session.findOne({ tokenHash: token });
+
+        if (!session || !session.isActive) {
+            return res.status(401).json({ message: "Sesión inválida" });
+        }
+
+        if (new Date() > session.expiresAt) {
+            return res.status(401).json({ message: "Sesión expirada" });
+        }
+
         req.user = decoded;
+        req.session = session;
         next();
     } catch (error) {
         return res.status(401).json({ message: "Token inválido" });
