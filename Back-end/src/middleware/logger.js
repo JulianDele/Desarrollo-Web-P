f0d27f0066ec88b8748fdc10ea967469487a9ba4
marijolinares <you@example.com>
2026-03-22 18:25:48 -0600

    DevOps: add logging middleware and logs directory

---
+++ Back-end/src/middleware/logger.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+const logFile = path.join(__dirname, '../../logs/app.log');
+
+const logger = (req, res, next) => {
+    const log = `${new Date().toISOString()} - ${req.method} ${req.url}\n`;
+
+    fs.appendFile(logFile, log, (err) => {
+        if (err) {
+            console.error("Error writing log:", err);
+        }
+    });
+
+    next();
+};
+
+module.exports = logger;
